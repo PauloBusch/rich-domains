0d4cb18c285124eb0a04c48492abd6f2850b8fac
paulobusch <paulo202015@outlook.com.br>
2020-05-24 17:53:45 -0300

    Testes nas queires

---
+++ Payment.Domain/Queries/StudantQueries.cs
@@ -0,0 +1,15 @@
+﻿using PaymentContext.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Payment.Domain.Queries
+{
+    public static class StudantQueries
+    {
+        public static Expression<Func<Student, bool>> GetStudantInfo(string document) { 
+            return x => x.Document.Number == document;    
+        }
+    }
+}
---
+++ Payment.Tests/Queries/StudantQueriesTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payment.Domain.Enums;
+using Payment.Domain.Queries;
+using Payment.Domain.ValueObjects;
+using PaymentContext.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Payment.Tests.Queries
+{
+    [TestClass]
+    public class StudantQueriesTests
+    {
+        private readonly List<Student> _students;
+        public StudantQueriesTests()
+        {
+            for (var i = 0; i < 10; i++){ 
+                var student = new Student(
+                    name: new Name("Paulo", "Busch"),
+                    document: new Document("1111111111" + i.ToString(), EDocumentType.CPF),
+                    email: new Email(i.ToString() + "@teste.com")
+                );
+                _students.Add(student);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldReturnNullWenDocumentNotExists()
+        {
+            var exp = StudantQueries.GetStudantInfo("212121");
+            var studant = _students.AsQueryable().Where(exp);
+            Assert.IsNull(studant);
+        }
+
+        [TestMethod]
+        public void ShouldReturnStudantWenDocumentExists()
+        {
+            var exp = StudantQueries.GetStudantInfo("11111111111");
+            var studant = _students.AsQueryable().Where(exp);
+            Assert.IsNotNull(studant);
+        }
+    }
+}
